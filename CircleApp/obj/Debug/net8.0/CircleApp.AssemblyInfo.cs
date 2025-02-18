c2e02db626b7aeadeca1d5b726a81e0ff91d44a8
Adam Suchi Hafizullah <adamsuchihafizullah@gmail.com>
2025-02-19 06:36:58 +0700

    update: add assets

--- CircleApp/obj/Debug/net8.0/CircleApp.AssemblyInfo.cs
+++ CircleApp/obj/Debug/net8.0/CircleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CircleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+213b73e7d06d205147eba79008f6848f19581f32")]
 [assembly: System.Reflection.AssemblyProductAttribute("CircleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CircleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
